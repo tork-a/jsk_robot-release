4865a9654ce9e770b7a31461f573e0a758831a96
Kei Okada <kei.okada@gmail.com>
2017-07-16 15:07:48 +0900

    Rebase from 'upstream'

--- jsk_robot_common/jsk_robot_startup/scripts/ImuRootlinkCalculator.py
+++ jsk_robot_common/jsk_robot_startup/scripts/ImuRootlinkCalculator.py
@@ -2,7 +2,7 @@
 
 import rospy
 import numpy
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, Vector3
 from sensor_msgs.msg import Imu
 import tf
 
@@ -12,7 +12,7 @@ class ImuRootlinkCalculator(object):
         # tf parameters
         self.base_link_frame = rospy.get_param("~base_link_frame", "BODY")
         self.listener = tf.TransformListener(True, rospy.Duration(10))
-        self.imu_sub = rospy.Subscriber("~input", Imu, self.imu_callback, queue_size = 10)
+        self.imu_sub = rospy.Subscriber("~input", Imu, self.imu_callback, queue_size = 1)
         self.pub = rospy.Publisher("~output", Imu, queue_size = 1)
 
     def execute(self):
@@ -25,25 +25,27 @@ class ImuRootlinkCalculator(object):
             self.initial_matrix = None
             
     def imu_callback(self, msg):
-        try:
-            self.listener.waitForTransform(msg.header.frame_id, self.base_link_frame, msg.header.stamp, rospy.Duration(1.0))
-            (trans,rot) = self.listener.lookupTransform(msg.header.frame_id, self.base_link_frame, msg.header.stamp)
-        except:
-            try:
-                rospy.logwarn("[%s] failed to solve imu_to_base tf in %f. Use rospy.Time(0): %s to %s", rospy.get_name(), stamp.to_sec(), msg.header.frame_id, self.base_link_frame)
-                (trans,rot) = self.listener.lookupTransform(msg.header.frame_id, self.base_link_frame, rospy.Time(0))
-            except:
-                rospy.logwarn("[%s] failed to solve imu_to_base tf: %s to %s", rospy.get_name(), msg.header.frame_id, self.base_link_frame)
-                return
+        raw_imu_quat = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        raw_imu_av = [msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z]
+        raw_imu_acc = [msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z]
 
-        # only convert orientation because acceleration and angular velocity are not used in ParticleOdometry
-        imu_rotation = tf.transformations.quaternion_matrix(rot)
-        imu_matrix = tf.transformations.quaternion_matrix([msg.orientation.x, msg.orientation.y,
-                                                           msg.orientation.z, msg.orientation.w])
-        imu_rootlink_quat = tf.transformations.quaternion_from_matrix(numpy.dot(imu_rotation, imu_matrix)) # base_link relative orientation
-        msg.orientation = Quaternion(*imu_rootlink_quat)
+        try:
+            self.listener.waitForTransform(self.base_link_frame, msg.header.frame_id, msg.header.stamp, rospy.Duration(1.0)) # gyrometer->body
+
+            trans = self.listener.asMatrix(self.base_link_frame, msg.header) # gyrometer->body
+            rootlink_quat = tf.transformations.quaternion_from_matrix(trans * tf.transformations.quaternion_matrix(raw_imu_quat))
+            rootlink_av = numpy.dot(trans[:3, :3], numpy.array(raw_imu_av))
+            rootlink_acc = numpy.dot(trans[:3, :3], numpy.array(raw_imu_acc))
+            # todo: convert covariance
+        except:
+            rospy.logwarn("[%s] failed to solve imu_to_base tf: %s to %s", rospy.get_name(), msg.header.frame_id, self.base_link_frame)
+            return
+
         msg.header.frame_id = self.base_link_frame
-        
+        msg.orientation = Quaternion(*rootlink_quat)
+        msg.angular_velocity = Vector3(*rootlink_av)
+        msg.linear_acceleration = Vector3(*rootlink_acc)
+                
         # publish
         self.pub.publish(msg)
         
